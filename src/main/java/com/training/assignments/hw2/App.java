8da6a1ba7f356ce773addac1fe7a87929a69492c
Richard Strickler <richastrick1998@gmail.com>
2022-08-21 13:47:31 -0500

    Initial Commit

---
+++ src/main/java/com/training/assignments/hw2/App.java
@@ -0,0 +1,13 @@
+package com.training.assignments.hw2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
