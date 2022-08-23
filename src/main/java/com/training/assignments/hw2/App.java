f6d074a22b7b275eebcf1186b887ea7e938c06ab
Richard Strickler <richastrick1998@gmail.com>
2022-08-22 21:39:35 -0500

    forgot to add these

--- src/main/java/com/training/assignments/hw2/App.java
+++ src/main/java/com/training/assignments/hw2/App.java
@@ -10,11 +10,24 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.zone.ZoneRulesException;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List; 
+
 
 /**
- * Hello world!
- *
+ * Java 8 lambdas, 
+ * stream functions {
+ * 		filter
+ * 		map
+ * 		findFirst (done)
+ * 		count
+ * }
+ * Date/Time (done)
+ * current time in different time zones (done)
+ * Arithmetic: + 8 hrs to current date time (done)
  */
+
 public class App 
 {	
     public static void main( String[] args ) throws IOException
@@ -23,14 +36,21 @@ public class App
     	
     	String country = "America";
     	String city = "Chicago";
+    	String response = "";
+    	Integer idNumber = 0;
     	int offset = 8;
     	Boolean validZone = false;
+    	Boolean validInput = false;
     	BufferedReader userInput;
-    	StringBuilder timeZoneID;
+    	StringBuilder timeZoneId;
     	String timeZone = "America/Chicago";
+    	List<TimeStampWithZone> timeStamps = new ArrayList<TimeStampWithZone>();
+    	
     	
     	while (!validZone) {
     		try {
+    			validInput = false;
+    			
     			userInput = new BufferedReader( new InputStreamReader(System.in));
     		 	System.out.println("\nPlease input the name of the Country (EX: America ):");
     		 	country = userInput.readLine();
@@ -39,22 +59,40 @@ public class App
      		 	System.out.println("\nPlease input the name of the City (EX: Chicago ):");
      		 	city = userInput.readLine();
      		 	
-	    		timeZoneID = new StringBuilder().append(country).append('/').append(city);
-	    		timeZone = timeZoneID.toString();
-	    		formatter.format(getZonedTime(timeZone));
+	    		timeZoneId = new StringBuilder().append(country).append('/').append(city);
+	    		timeZone = timeZoneId.toString();
+	    		timeStamps.add(
+	    				new TimeStampWithZone(formatter.format(getZonedTime(timeZone)), timeZoneId.toString(), idNumber)
+	    				);
+	    		
 	    		validZone = true;
 	    	}
 	    	catch(ZoneRulesException e){
-	    		System.out.println("\nEither the Country or City names are not valid options.");
+	    		System.out.println("\nEither the Country or City names are invalid options.");
 	    		validZone = false;
 	    	}
+    		while (!validInput) {
+	    		userInput = new BufferedReader( new InputStreamReader(System.in));
+	    		System.out.println("\nEnter another TimeZone? (Y/N):");
+	    		response = userInput.readLine();
+    		
+	    		if (response.equals("Y") || response.equals("y")) {validZone = false; validInput = true;}
+	    		else if (response.equals("N") || response.equals("n")) {validZone = true; validInput = true;}
+    		}
+    		idNumber++;
     		
     	}
-    	
+    	System.out.println(
+    	Stream.of(timeStamps.toArray())
+    	.findFirst()
+        .toString()
+        );
     	
     	System.out.println(formatter.format(getZonedTime(timeZone)));
     	
     	System.out.println(formatter.format(getZonedTime(timeZone).plusHours(offset)));
+    	
+    	
     	
     	
     }
